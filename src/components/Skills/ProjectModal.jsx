223c825c7c0ad089014d710e04a52797df2b8402
jordiciurana16 <jciuranas@gmail.com>
2023-08-17 14:02:31 +0200

    17/08/2023

--- src/components/Skills/ProjectModal.jsx
+++ src/components/Skills/ProjectModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BsX } from 'react-icons/bs';
+import styles from './ProjectModal.module.css'
 
 const ProjectModal = ({ project, onClose }) => {
   const handleCloseModal = () => {
@@ -7,21 +8,39 @@ const ProjectModal = ({ project, onClose }) => {
   };
 
   return (
-    <div className={`card`}>
-      <div className='card-header d-flex justify-content-between align-items-center'>
+    <div className={`card ${styles.projectsModal}`}>
+      <div className={`card-header d-flex justify-content-between align-items-center`}>
         <div className='d-flex align-items-center'>
-          <h3 className='mb-0'>{project.skill}</h3>
+          <h3 className={`mb-0 ${styles.projectTitle}`}>{project.skill} Projects</h3>
         </div>
         <div
-          className='fs-1 d-flex align-items-center'
+          className={`${styles.closeModal} fs-1 d-flex align-items-center`}
           onClick={handleCloseModal}
         >
           <BsX />
         </div>
       </div>
-      <div className='card-body'>
-        <p dangerouslySetInnerHTML={{ __html: project.description }} />
-      </div>
+      <div className={`card-body ${styles.projectList}`}>
+  <ul className='list-unstyled'>
+    {project.projects.map((project, index) => (
+      <li key={index}>
+        {/* Render the title as a link */}
+        <h5>
+          <a href={project.link} target='_blank' rel='noopener noreferrer'>
+            {project.title}
+          </a>
+        </h5>
+        <p>{project.description}</p>
+      </li>
+    ))}
+  </ul>
+</div>
+
+
+
+
+
+
     </div>
   );
 };
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import skillsData from '../../data/skillsData.json';
 import Separator from '../Separator/Separator';
 import styles from './Skills.module.css';
@@ -8,6 +8,7 @@ const Skills = () => {
   const [isExpanded, setExpanded] = useState(false);
   const [hoveredSkill, setHoveredSkill] = useState(null);
   const [selectedSkill, setSelectedSkill] = useState(null); // Add state for the selected skill
+  const [showOverlay, setShowOverlay] = useState(false);
 
   const highlightWords = (text, highlightedWords) => {
     let highlightedText = text;
@@ -20,55 +21,80 @@ const Skills = () => {
 
   const handleOpenModal = (skill) => {
     setSelectedSkill(skill);
+    setShowOverlay(true);
+
   };
 
   const handleCloseModal = () => {
     setSelectedSkill(null);
+    setShowOverlay(false);
+
   };
 
+  // Add event listener to close the modal when clicked outside
+  useEffect(() => {
+    const handleOverlayClick = (event) => {
+      if (event.target.classList.contains('overlay')) {
+        handleCloseModal();
+      }
+    };
+
+    if (showOverlay) {
+      document.addEventListener('click', handleOverlayClick);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleOverlayClick);
+    };
+  }, [showOverlay]);
+
   return (
-    <div
-      onMouseEnter={() => setExpanded(true)}
-      onMouseLeave={() => setExpanded(false)}
-    >
-      <h2>SKILLS</h2>
-      <Separator expanded={isExpanded} />
+    <>
+      {/* Overlay */}
+      {showOverlay && <div className='overlay'></div>}
+      <div
+        onMouseEnter={() => setExpanded(true)}
+        onMouseLeave={() => setExpanded(false)}
+      >
+        <h2>SKILLS</h2>
+        <Separator expanded={isExpanded} />
 
-      {skillsData.map((skills, index) => (
-        <React.Fragment key={index}>
-          <div
-            className={`rounded pt-2 ps-2 pe-2 ${styles.skillsDiv} ${hoveredSkill === skills.skill ? styles.hovered : ''}`}
-            onMouseEnter={() => setHoveredSkill(skills.skill)}
-            onMouseLeave={() => setHoveredSkill(null)}
-          >
-            <div className="d-flex justify-content-between align-items-center">
-              <h6>{skills.skill}</h6>
-              <a
-                href="#projects"
-                className={`text-black ${styles.projectsLink}`}
-                onClick={() => handleOpenModal(skills)}
-              >
-                Projects
-              </a>
+        {skillsData.map((skills, index) => (
+          <React.Fragment key={index}>
+            <div
+              className={`rounded pt-2 ps-2 pe-2 ${styles.skillsDiv} ${hoveredSkill === skills.skill ? styles.hovered : ''}`}
+              onMouseEnter={() => setHoveredSkill(skills.skill)}
+              onMouseLeave={() => setHoveredSkill(null)}
+            >
+              <div className="d-flex justify-content-between align-items-center">
+              <h6>{skills.skill.toUpperCase()}</h6>
+                <a
+                  href="#projects"
+                  className={`text-black ${styles.projectsLink}`}
+                  onClick={() => handleOpenModal(skills)}
+                >
+                  Projects
+                </a>
+              </div>
+              <p
+                className='mt-2'
+                dangerouslySetInnerHTML={{
+                  __html: hoveredSkill === skills.skill
+                    ? highlightWords(skills.description, skills.highlight)
+                    : skills.description
+                }}
+              />
             </div>
-            <p
-              className='mt-2'
-              dangerouslySetInnerHTML={{
-                __html: hoveredSkill === skills.skill
-                  ? highlightWords(skills.description, skills.highlight)
-                  : skills.description
-              }}
-            />
-          </div>
-          {index !== skillsData.length - 1 && <hr className="skills-divider" />}
-        </React.Fragment>
-      ))}
+            {index !== skillsData.length - 1 && <hr className="skills-divider" />}
+          </React.Fragment>
+        ))}
 
-      {/* Render the ProjectModal when a skill is selected */}
-      {selectedSkill && (
-        <ProjectModal project={selectedSkill} onClose={handleCloseModal} />
-      )}
-    </div>
+        {/* Render the ProjectModal when a skill is selected */}
+        {selectedSkill && (
+          <ProjectModal project={selectedSkill} onClose={handleCloseModal} />
+        )}
+      </div>
+    </>
   );
 };
 
