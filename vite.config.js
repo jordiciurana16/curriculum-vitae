b5ad253ae188898ff8b9f388c29a6fc500eaac49
jordiciurana16 <jciuranas@gmail.com>
2023-08-22 13:35:55 +0200

    ss

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/curriculum-vitae/"
 })
+
+
+// https://www.youtube.com/watch?v=igCO5i4NnfU //
